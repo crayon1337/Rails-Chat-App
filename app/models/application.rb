4a42c7312e6287825b93690b373906de779f5142
Crayon <darkspear123@outlook.com>
2019-10-26 02:57:31 +0200

    Revert "Revert "Added chat-add to application functionality""
    This reverts commit 84bbd8631cfab826e6c1af382023f8717d791114.

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,9 @@ class Application < ApplicationRecord
 
     #validate the name input (Required from the client!)
     validates :name, presence: true, length: {minimum: 3}
+
+    #Set the route param
+    def to_param 
+        token
+    end
 end
